b74e8faa1052adb7352dd2e23e35ec067518647d
starklocs <laura.carvalho@alu.unibalsas.edu.br>
2024-12-05 14:04:37 -0300

    Ajustando Front, Back, e Containers

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
 from guess import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
